4cd5a94828d7e6e8a3e94090b4c39af72679d122
divyanshuk26 <dkotangale123@gmail.com>
2025-09-15 10:49:33 +0530

    Add files via upload

--- health.java
+++ health.java
@@ -3,6 +3,7 @@ package Driver;
 import static org.testng.Assert.assertEquals;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -39,33 +40,39 @@ public class health {
 		Assert.assertTrue(title(), "title and url not matched");
 		
 	}
-	@Test
+	@Test(priority=1)
 	public void login() throws InterruptedException {
 		driver.findElement(By.xpath("//*[@id=\"username_id\"]")).sendKeys("admin");
 		driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("pass123");
 		driver.findElement(By.xpath("//*[@id=\"login\"]")).click();
-
-		//Testcase 2
-		// Verify pop up and proceed to the next page in the new 1 counter page.
-		//		Thread.sleep(2000);
-//		WebElement clickOnBill = driver.findElement(By.cssSelector("body > my-app > div > div > div.page-container > div.page-sidebar-wrapper > div > ul > li:nth-child(9) > a > span.title"));
-//		clickOnBill.click();
-//		Thread.sleep(2000);
-//		WebElement verifyPopUp = driver.findElement(By.xpath("//span[text()='Select Counter']"));
-//		String popTitle = verifyPopUp.getText();
-//		System.out.println(popTitle);
 		}
 	
-	@Test 
-	public void keybordAction() {
-		driver.get("https://healthapp.yaksha.com/Home/Index#/Billing/SearchPatient");
-		driver.findElement(By.xpath("/html[1]/body[1]/my-app[1]/div[1]/div[1]/div[3]/div[2]/div[1]/div[1]/scheme-refund[1]/change-billing-counter[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/h5[1]")).click();
-		Actions action = new Actions(driver);
-		 action.keyDown(Keys.ALT).sendKeys("n").keyUp(Keys.ALT).build().perform();
-		
-		
-		
+	 
+	public boolean openVerificationMenu() {
+	    try {
+	        JavascriptExecutor js = (JavascriptExecutor) driver;
+	        WebElement verificationTab = driver.findElement(By.xpath("/html/body/my-app/div/div/div[3]/div[1]/div/ul/li[33]/a/span[1]"));
+	        
+	        // Scroll till element visible
+	        js.executeScript("arguments[0].scrollIntoView(true);", verificationTab);
+	        
+	        // Click
+	        verificationTab.click();
+	        return true;
+
+	    } catch (Exception e) {
+	        System.err.println("openVerificationMenu failed: " + e.getMessage());
+	        return false;
+	    }
 	}
+	
+	@Test(priority=2)
+	public void verficationScroll() {
+		Assert.assertTrue(openVerificationMenu(), "Verification not found ");
+	}
+	
+	
+
 //	@AfterClass
 //	public void teardown() {
 //		driver.quit();
